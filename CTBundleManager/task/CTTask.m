fa391fbe970368816baee108a4dd816888a30133
tczhu <tczhu@Ctrip.com>
2018-02-06 12:58:18 +0800

    【高】update

--- CTBundleManager/task/CTTask.m
+++ CTBundleManager/task/CTTask.m
@@ -19,7 +19,9 @@
     task.launchPath = launchPath;
     task.arguments = arguments;
     task.currentDirectoryPath = workspace;
-    task.environment = @{@"LANG":@"zh_CN.UTF-8"};
+    task.environment = @{@"LANG":@"zh_CN.UTF-8"
+                         @"PATH"@"/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:/usr/local/"
+                         };
     
     NSPipe* outputPipe = [NSPipe pipe];
     [task setStandardOutput:outputPipe];
